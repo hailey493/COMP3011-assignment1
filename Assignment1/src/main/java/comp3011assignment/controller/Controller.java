b864055a79eb29a5924c672eaf78114212e1613b
haileydas <leehailey2016@gmail.com>
2026-09-01 00:34:44 +0930

    basic transcript and start, stop button indication

--- Assignment1/src/main/java/comp3011assignment/controller/Controller.java
+++ Assignment1/src/main/java/comp3011assignment/controller/Controller.java
@@ -7,24 +7,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.audio.transcriptions.TranscriptionCreateParams;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
 	
-	@PostMapping(value = "/transcribe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadAudio(@RequestParam("audio") MultipartFile file) {
+	@PostMapping(value = "/audio/transcribe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadAudio(@RequestParam("audio") MultipartFile file) 
+			throws IOException{
         if (file.isEmpty()) {
-            return ResponseEntity.badRequest().body("Please upload a valid audio file.");
+        	String emptyFile = "Please upload a valid audio file.";
+            return emptyFile;
         }
-
+        Path tempFile = null;
         try {
+        	OpenAIClient client = OpenAIOkHttpClient.fromEnv();
+            tempFile = Files.createTempFile("audio-","-" + file.getOriginalFilename());
             
-        	System.out.println("Audio received by backend"); // testing to see if the audio is received
-            return ResponseEntity.ok("Audio received");
+        	file.transferTo(tempFile);
+        	
+        	var params = TranscriptionCreateParams.builder()
+                    .file(tempFile)
+                    .model("gpt-4o-mini-transcribe")
+                    .build();
+
+            var result = client
+                    .audio()
+                    .transcriptions()
+                    .create(params);
+            
+            return result.asTranscription().text();
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Failed to upload file.");
+        	System.out.println("OpenAI error type: " + e.getClass().getName());
+            System.out.println("OpenAI error message: " + e.getMessage());
+            
+            String failUpload = "Failed to upload file";
+            return failUpload;
+        } 
+        finally {
+        	if (tempFile != null) {
+        		Files.deleteIfExists(tempFile);
+        	}
         }
     }
+	
 }
